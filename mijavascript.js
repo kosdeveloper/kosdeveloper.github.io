971965bf42cef3bbf5a5d7694b9b85ffb15ddc85
kosdeveloper <70531028+kosdeveloper@users.noreply.github.com>
2024-02-15 13:22:38 +0100

    Add files via upload

--- mijavascript.js
+++ mijavascript.js
@@ -1,23 +1,5 @@
-//seleccionamos div
-let div = document.getElementsByTagName("div")[0];
+document.cookie = "idioma=es";
+document.cookie = "esquemaColor=dark";
+document.cookie = "haVotadoEncuesta=si";
 
-//Solicitamos al usuario los valores de los atributos
-//y modificamos sus atributos data
-div.dataset.ancho = prompt("Ingrese el ancho de la caja:");
-div.dataset.alto = prompt("Ingrese el alto de la caja:");
-div.dataset.color_borde = prompt("Ingrese el color del borde de la caja:");
-div.dataset.color_texto = prompt("Ingrese el color del texto:");
-div.dataset.color_fondo = prompt("Ingrese el color del fondo:");
-
-// Aplicamos los estilos a la caja
-div.style.width = div.dataset.ancho + 'px';
-div.style.height = div.dataset.alto + 'px';
-div.style.border = '3px solid ' + div.dataset.color_borde;
-div.style.color = div.dataset.color_texto;
-div.style.backgroundColor = div.dataset.color_fondo;
-
-//mostramos por consola la estructura interna del dataset
-console.info(div.dataset); 
-//mostramos por consola los valores de los atributos
-for(atributo of div.attributes)
-    console.log(atributo);
+console.log("Todas las Cookies:", document.cookie);
