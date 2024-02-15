fa2712b229513695b07174f8050ffd289d19f128
kosdeveloper <70531028+kosdeveloper@users.noreply.github.com>
2024-02-15 16:47:31 +0100

    Add files via upload

--- cookie05/creacookie.js
+++ cookie05/creacookie.js
@@ -2,4 +2,6 @@ document.cookie = "idioma=es";
 document.cookie = "esquemaColor=dark";
 document.cookie = "haVotadoEncuesta=si";
 
+document.cookie = `esquemaColor=dark;max-age=${5}`;
+
 console.log("Cookies creadas:", document.cookie);
