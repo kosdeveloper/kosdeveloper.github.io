827431bcee0aee919bf89738946f4a66259487f9
kosdeveloper <70531028+kosdeveloper@users.noreply.github.com>
2024-02-15 19:03:42 +0100

    Add files via upload

--- cookie05/borracookie.js
+++ cookie05/borracookie.js
@@ -1,13 +1,13 @@
 // Función para borrar una cookie por su nombre
 function borrarCookie(nombre) {
-  document.cookie = nombre + '=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/;';
+  document.cookie = nombre + '=; expires=Thu, 01 Jan 1970 00:00:00 GMT';
 }
 // Borrar las cookies especificadas
 borrarCookie("idioma");
 borrarCookie("esquemaColor");
 borrarCookie("haVotadoEncuesta");
 
-document.cookie = "idioma=es; expires=Thu, 18 Dec 2013 12:00:00 UTC";
+//document.cookie = "idioma=es; expires=Thu, 18 Dec 2013 12:00:00 UTC";
 // Imprimir las cookies después de borrarlas (esto debería mostrar una cadena vacía)
 console.log("Cookies borradas: ", document.cookie);
 
