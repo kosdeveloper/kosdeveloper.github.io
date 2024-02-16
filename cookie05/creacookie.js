376d685cc2439988dfad0315108fe4198dc6f938
kosdeveloper <70531028+kosdeveloper@users.noreply.github.com>
2024-02-16 09:27:07 +0100

    Delete cookie05 directory

--- cookie05/creacookie.js
+++
@@ -1,7 +0,0 @@
-document.cookie = "idioma=es";
-document.cookie = "esquemaColor=dark";
-document.cookie = "haVotadoEncuesta=si";
-
-document.cookie = `esquemaColor=dark;max-age=${20}`;
-
-console.log("Cookies creadas:", document.cookie);
