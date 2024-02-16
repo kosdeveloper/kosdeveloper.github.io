2d377c235e17e42e24da194d151d71fa3d84cabb
kosdeveloper <70531028+kosdeveloper@users.noreply.github.com>
2024-02-16 12:40:08 +0100

    Add files via upload

---
+++ cookie/creacookie.js
@@ -0,0 +1,7 @@
+//crear cookies
+document.cookie = "idioma=es";
+document.cookie = "esquemaColor=dark;max-age=20";
+document.cookie = "haVotadoEncuesta=si";
+document.cookie = "seSuscribe=no";
+
+console.log("Cookies creadas: ", document.cookie);
